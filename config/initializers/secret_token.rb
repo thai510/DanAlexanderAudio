957a9fbe9aa80ed0e32e7a62502f1f81339cc360
Ian Alexander <thai510@gmail.com>
2012-03-17 17:39:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DanAlexanderAudio::Application.config.secret_token = '931fb29b433703a56ec4931aba23e1a14974155238df1a928e53d1aa53589237304408cf5e0a9e4c0446002c047d0459a56d4166d641413a5b36873dfdb0a34b'
